d399f26852a6a83e0fd2e3e1759b89e4175ad78b
VadaszDaniel2006 <vadaszdana@gmail.com>
2025-11-05 10:32:51 +0100

    Correct JSON parsing and error handling in getUsers
    Fix the fetch call to correctly parse JSON and update the DOM with user data.

--- client.js
+++ client.js
@@ -9,9 +9,28 @@ const usersData = document.getElementById('usersData') //AZ outpot tábla törzs
 async function getUsers() {
     try{
         const response = await fetch(apiUrl); //Kapcsolódás az API-hoz
-        const users = await response.json
+        const users = await response.json();  //Az adatok lekérése APi-ról (a users egy tömb)
+        
+        //A user egy objektum
+        usersData.innerHTML = users.map(user => ` 
+            <tr>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.city}</td>
+            <td>${user.address}</td>
+            <td>${user.phone}</td>
+            <td>${user.email}</td>
+            <td>${user.gender}</td>
+            <td>${user.actions}</td>
+            </tr>
+            `).join('');
     }
     catch(e) {
+        console.error(e.message); //Hiba üzenet küldése a konzolra fejlesztéshez
+        alert ('Hiba történt az adatok elérése során!');
 
     }
 }
+
+getUsers(); //Az adatok lekérése szolgáló függvény meghívása
